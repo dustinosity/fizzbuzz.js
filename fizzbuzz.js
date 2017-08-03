77e446c8760189b5ce181b94306d48cf8c106e51
Dustin Selman <dselman@gmail.com>
2017-08-03 10:26:28 -0700

    small change

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,7 +3,7 @@
  * http://github.com/dsel/fizzbuzz.js
  */
  
-const fizzBuzz = (x, y, ans) => x % y ? '' : ans;
+const fizzBuzz = (x, y, answer) => x % y ? '' : answer;
 
 for (let n = 1; n <= 100; n++) {
   console.log(fizzBuzz(n, 3, 'Fizz') + fizzBuzz(n, 5, 'Buzz') || n);
